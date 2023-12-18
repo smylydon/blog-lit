b9e06aaccec28330efa11362dc7260f85677ae9a
MSI\smyly <smylydon@yahoo.com>
2023-12-17 20:16:48 -0800

    index.html and index.css are now automagically moved to dist folder.

--- rollup.config.js
+++ rollup.config.js
@@ -8,12 +8,14 @@ import summary from 'rollup-plugin-summary';
 import {terser} from 'rollup-plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
+import copy from 'rollup-plugin-copy';
 
 export default {
   input: 'mg-blog.js',
   output: {
     file: 'mg-blog.bundled.js',
     format: 'esm',
+    sourcemap: true,
   },
   onwarn(warning) {
     if (warning.code !== 'THIS_IS_UNDEFINED') {
@@ -32,6 +34,12 @@ export default {
           regex: /^__/,
         },
       },
+    }),
+    copy({
+      targets: [
+        {src: 'src/index.html', dest: 'dist'},
+        {src: 'src/index.css', dest: 'dist'},
+      ],
     }),
     summary(),
   ],
